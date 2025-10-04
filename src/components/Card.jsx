425cab039bf6c28a689ee54836d64c0979ceb037
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-05 01:31:02 +0500

    theme

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,10 @@ import Button from './Button'
 export const Card = ({theme}) => {
   return (
     <>
-    <h2>Your Theme</h2>
-    <Button BtnName={theme} BtnColor={theme} textColor='#fff'/>
+    <div className='bg-white p-5 shadow-2xl rounded-2xl flex flex-col justify-center items-start h-[150px] w-[270px]'>
+    <h2 className='text-2xl font-bold mb-3'>Your Theme</h2>
+    <Button BtnName={theme} BtnColor={theme} textColor='#AFDBF5' />
+    </div>
     </>
   )
 }
