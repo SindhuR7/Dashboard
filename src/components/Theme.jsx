339b53b07116da8d6ebf5e7eb36ade7d51261c1a
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-05 01:31:56 +0500

    Theme Picker

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -4,15 +4,20 @@ const Theme = ({setTheme}) => {
 
   return (
     <>
-    <h1>Theme Picker</h1>
-    <select onChange={(e) => setTheme(e.target.value)}>
+    <div className="bg-white p-5  rounded-2xl flex flex-col  items-start h-[150px] w-[270px] shadow-2xl">
+    <h1 className="text-2xl font-bold mb-4">Theme Picker</h1>
+    <select onChange={(e) => setTheme(e.target.value)} className="border bg-transparent px-2 py-1 text-xl rounded-2xl outline-none">
+        <option aria-readonly >--Select any Color--</option>
         <option value="Red">Red</option>
-        <option value="Green">Green</option>
-        <option value="Yellow">Yellow</option>
+        <option value="Chartreuse">Chartreuse</option>
+        <option value="Chocolate">Chocolate</option>
         <option value="Blue">Blue</option>
         <option value="Orange">Orange</option>
-        <option value="Black">Black</option>
+        <option value="Purple">Purple</option>
+        <option value="Violet">Violet</option>
+        <option value="GreenYellow">GreenYellow</option>
     </select>
+    </div>
     </>
   )
 }
